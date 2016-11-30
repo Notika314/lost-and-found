9dab99ad3b9fa1995d92fb9def06e971f8e27777
Notika314 <notika314@gmail.com>
2016-11-30 09:52:32 -0500

    create user and rting table

--- db/migrate/20161129204917_create_users.rb
+++ db/migrate/20161129204917_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-    	t.string :username
-    	t.string :password_digest
+    	t.string :username , null: false
+    	t.string :password_digest, null: false
     	t.string :phone
-    	t.string :email
+    	t.string :email, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,19 @@ ActiveRecord::Schema.define(version: 20161129205326) do
   enable_extension "plpgsql"
 
   create_table "ratings", force: :cascade do |t|
+    t.integer  "stars",      null: false
+    t.integer  "user_id",    null: false
+    t.integer  "rater_id",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_ratings_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password_digest"
+    t.string   "username",        null: false
+    t.string   "password_digest", null: false
     t.string   "phone"
-    t.string   "email"
+    t.string   "email",           null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
